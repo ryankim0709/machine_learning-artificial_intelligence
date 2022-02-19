0c12d9a743cbfce28bd8cebb5424a63ba44b0582
Ryan Kim <Ryan@RyanMacbook-2.local>
2022-02-19 15:33:17 -0800

    Random Forest Classifier

--- Random_Forest_Classifier/main.py
+++ Random_Forest_Classifier/main.py
@@ -8,3 +8,22 @@ print(exo_test_df.shape) # => (570, 3198)
 
 print(exo_test_df["LABEL"].value_counts()) # Counts the amount of distint values in the "LABEL" column
 
+x_train = exo_train_df.iloc[:,1:] 
+y_train = exo_train_df.iloc[:, 0]
+print("X training data",x_train)
+print("Y training data", y_train)
+
+rf_clf = RandomForestClassifier(n_jobs=-1, n_estimators=50)
+rf_clf.fit(x_train, y_train) # Fitting the model
+print(rf_clf.score(x_train, y_train)) # Seeing how accurate this model is
+
+# Now we must make predictions using this trained model
+x_test = exo_test_df.iloc[:,1:]
+y_test = exo_test_df.iloc[:,0]
+
+prediction = rf_clf.predict(x_test) # Predicting what we would get for x_test data set from our trained tree
+print(prediction)
+print(type(prediction)) # => numpy.ndarray
+
+prediction = pd.Series(prediction) # If we convert to pandas series
+print(prediction.value_counts()) # We can see our prediction results
--- Random_Forest_Classifier/readme.md
+++ Random_Forest_Classifier/readme.md
@@ -6,6 +6,14 @@ A decision tree is a **flow chart** which separates data based on sets of condit
 
 **About the Random Forest Classifier**
 <br/>
+**Setting it up**
+To set up the Random FOrest Classifier, we just initialize `rf_clf = RandomForestClassifier(n_jobs = -1, n_estimators = 50)`
+The n_jobs parameter is how many jobs to run in parallel. - fit - predict - decision_path - path
+`n_jobs = -1` means to use all processors
+
+## The n_estimators is how many trees to have in the forest
+
+**Predicting using it**
 The Random Forest Classifier has a **fit()** function which takes in two inputs.
 
 1. Collection of feature variables
