1a141d07226963dd0519de75da74b9819eb3b111
Ryan Kim <Ryan@RyanMacbook-2.local>
2022-02-22 14:07:46 -0800

    Mean Normalization

--- Data_Normalization/main.py
+++ Data_Normalization/main.py
@@ -7,8 +7,8 @@ def mean_normalise(series):
     norm_series = (series - series.mean()/(series.max() - series.min()))
     return norm_series
 
-exo_train_df = pd.read_csv('https://s3-student-datasets-bucket.whjr.online/whitehat-ds-datasets/kepler-exoplanets-dataset/exoTrain.csv')
-exo_test_df = pd.read_csv('https://s3-student-datasets-bucket.whjr.online/whitehat-ds-datasets/kepler-exoplanets-dataset/exoTest.csv')
+exo_train_df = pd.read_csv('https://s3-student-datasets-bucket.-.online/--ds-datasets/kepler-exoplanets-dataset/exoTrain.csv')
+exo_test_df = pd.read_csv('https://s3-student-datasets-bucket.-.online/--ds-datasets/kepler-exoplanets-dataset/exoTest.csv')
 
 print(exo_train_df.shape) 
 print(exo_test_df.shape)
---
+++ Transformation/main.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+
+exo_train_df = pd.read_csv('https://s3-student-datasets-bucket.whjr.online/whitehat-ds-datasets/kepler-exoplanets-dataset/exoTrain.csv')
+exo_test_df = pd.read_csv('https://s3-student-datasets-bucket.whjr.online/whitehat-ds-datasets/kepler-exoplanets-dataset/exoTest.csv')
